6ae8ba2d5ad0ae6568f9f7acfee5f76d7635de21
Raphaël Vinot <raphael@vinot.info>
2015-05-11 17:03:28 +0200

    Update deps

--- update_deps.sh
+++ update_deps.sh
@@ -5,8 +5,8 @@ set -x
 
 DEST_DIR="web/static/"
 
-ANGULAR='1.3.14'
-ANGULAR_BOOTSTRAP='0.12.1'
+ANGULAR='1.3.15'
+ANGULAR_BOOTSTRAP='0.13.0'
 
 wget https://ajax.googleapis.com/ajax/libs/angularjs/${ANGULAR}/angular.min.js -O ${DEST_DIR}/angular.min.js
 wget https://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-${ANGULAR_BOOTSTRAP}.min.js -O ${DEST_DIR}/ui-bootstrap-tpls.min.js
