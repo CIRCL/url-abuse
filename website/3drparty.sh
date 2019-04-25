bd00995fa9a9cb3ec91c7e187ddacb2ce32fe87a
Raphaël Vinot <raphael@vinot.info>
2019-04-25 14:08:46 +0200

    chg: Bump deps

--- website/3drparty.sh
+++ website/3drparty.sh
@@ -5,7 +5,7 @@ set -x
 
 DEST_DIR="web/static/"
 
-ANGULAR='1.7.5'
+ANGULAR='1.7.8'
 ANGULAR_BOOTSTRAP='2.5.0'
 
 wget https://ajax.googleapis.com/ajax/libs/angularjs/${ANGULAR}/angular.min.js -O ${DEST_DIR}/angular.min.js
